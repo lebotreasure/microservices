636f757752877c059a153601e31b5939198ac7d1
lebotreasure <lebohangsylviak@gmail.com>
2024-08-30 07:34:06 +0200

    Add doctors service

---
+++ doctors-service/src/test/java/com/lebohangkhaeane/doctors_service/DoctorsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lebohangkhaeane.doctors_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
